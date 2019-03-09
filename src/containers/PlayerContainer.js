97d14853d212d856a29102b1c68c72f4af95b74f
Brendan Jaffary <brendanjaffary@gmail.com>
2019-03-08 23:29:03 -0800

    Created PlayerContainer and Page

--- src/containers/PlayerContainer.js
+++ src/containers/PlayerContainer.js
@@ -1,24 +1,48 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { Container, Row, Col } from "reactstrap";
-import PlayerList from "../components/PlayerList/PlayerList";
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Container, Row, Col } from 'reactstrap'
+import PlayerList from '../components/PlayerList/PlayerList'
+import PlayerForm from '../components/PlayerForm/PlayerForm'
 
 class PlayerContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    return (
-      <Container>
-        <PlayerList />
-      </Container>
-    );
-  }
+   constructor(props) {
+      super(props)
+      this.state = {
+         addingChar: false,
+         editingChar: false,
+      }
+   }
+   toggleAddForm = () => {
+     this.setState({
+       addingChar : !this.state.addingChar
+     })
+   }
+
+   toggleEditForm = () => {
+     this.setState({
+       editingChar: true,
+     })
+   }
+
+   render() {
+      return (
+         <Container fluid>
+            <Row>
+               <Col sm="12" md="4">
+                  <PlayerList addPlayer={this.toggleAddForm} players={this.props.players} />
+               </Col>
+               <Col sm="12" md="8">
+                  {this.state.addingChar && <PlayerForm toggle={this.toggleAddForm} newChar={true}/>}
+                  {this.state.editingChar && <PlayerForm  newChar={false}/>}
+               </Col>
+            </Row>
+         </Container>
+      )
+   }
 }
 
 const mapStateToProps = state => ({
-  players : state.campaign.loadedCampaign.characters.players
+   players: state.campaign.loadedCampaign.characters.players,
 })
 
-export default connect(mapStateToProps)(PlayerContainer);
+export default connect(mapStateToProps)(PlayerContainer)
