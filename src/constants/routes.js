cdc23100a3291641f600aca924e573388eefd3ee
Brendan Jaffary <brendanjaffary@gmail.com>
2019-04-27 12:54:01 -0700

    Adjusted routing between containers

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,5 +1,7 @@
 export const LANDING_PAGE = "/";
 export const NPC_PAGE = "/campaign/saved-npcs"
+export const NPC_TEMPLATE_LIST = "/campaign/npc-templates"
+export const NPC_FORM = '/campaign/npc-form/'
 export const PLAYER_PAGE = "/campaign/saved-players"
 export const ENCOUNTERS_PAGE = "/campaign/saved-encounters"
 export const CAMPAIGN_MANAGER = "/campaignmanager/:campaignName";
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 export { default as Campaign } from "./Campaign";
 export { default as LandingPage } from "./LandingPage";
-export { default as NpcPage } from "./NpcPage";
 export { default as PlayerPage } from "./PlayerPage";
 export { default as EncounterPage } from "./EncounterPage";
