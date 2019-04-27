22ef3a50237f14b787e68d9bd3977d0f08c6f4e8
Brendan Jaffary <brendanjaffary@gmail.com>
2019-04-27 12:55:22 -0700

    Added StatBlock component with own CSS and Utils

---
+++ src/utils/statBlockUtils.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-useless-escape */
+import { skills } from '../constants/npcInformation'
+
+export function savingThrows(npc) {
+   const list = []
+   if (npc.strength_save) {
+      list.push(`Str +${npc.strength_save}`)
+   }
+   if (npc.dexterity_save) {
+      list.push(`Dex +${npc.dexterity_save}`)
+   }
+   if (npc.constitution_save) {
+      list.push(`Con +${npc.constitution_save}`)
+   }
+   if (npc.wisdom_save) {
+      list.push(`Wis +${npc.wisdom_save}`)
+   }
+   if (npc.intelligence_save) {
+      list.push(`Int +${npc.intelligence_save}`)
+   }
+   if (npc.charisma_save) {
+      list.push(`Cha +${npc.charisma_save}`)
+   }
+   if (list.length > 0) {
+      return list.join(', ')
+   } else {
+      return null
+   }
+}
+
+export function skillList(npc) {
+   const list = []
+   skills.forEach(skill => {
+      if (npc[skill.id]) {
+         list.push(`${skill.name} +${npc[skill.id]}`)
+      }
+   })
+   if (list.length > 0) {
+      return list.join(', ')
+   } else {
+      return null
+   }
+}
+
+export function spellList(ability) {
+   const arr = ability.split(/(?:\•)/g)
+   return arr
+}
